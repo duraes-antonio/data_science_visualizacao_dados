121f595f7b30f95f66c8c483a2d2b69781d48626
duraes-antonio <Avanteg6!>
2020-08-30 13:55:27 -0300

    REFACT: Uso de modelo de gráfico 2d, chamada de funções e finalização dos gráficosd

--- src/main.py
+++ src/main.py
@@ -1,77 +1,121 @@
 import os.path as path
 from typing import List, Dict
 
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sb
-from pandas import DataFrame
+
+from graph import *
 
 
 def export_each_column_to_csv(file_read_path: str, colums_names: List[str], sep=';'):
     df = pd.read_csv(file_read_path, sep)
-    dirname = path.dirname(file_read_path) + path.sep
+    dirname = path.dirname(file_read_path)
 
     for col in colums_names:
-        df[col].to_csv(f"{dirname}{col}.csv", sep, index=False, header=True)
-
-
-def plot_bar_graph(df: DataFrame, column_x_name: str):
-    sb.countplot(data=df, x=column_x_name, palette=sb.color_palette('YlOrRd'))
-    plt.show()
-
-
-def plot_histogram(df: DataFrame):
-    sb.distplot(df, kde=False)
-    plt.show()
+        df[col].to_csv(f"{path.join(dirname, col)}.csv", sep, index=False, header=True)
 
 
 def str_num_to_float(num_str: str) -> float:
     return float(num_str.split()[0].replace(',', '.'))
 
 
-def plot_pie_graph(labels: List[str], values: List[int]):
-    explode = [0 for i in range(len(labels))]
-    fig1, ax1 = plt.subplots()
-    ax1.axis('equal')
-    ax1.pie(values, explode, labels, autopct='%1.1f%%', startangle=30, labeldistance=1.05)
-    plt.show()
-
-
 def main():
     sb.set_style('darkgrid')
     dirname = '../data/microdados_enade_2018/2018/3.DADOS'
-    sep = ';'
-    # dirname_file_full = f'{dirname}/microdados_enade_2018.csv'
-    cols = ['CO_RS_I1', 'CO_RS_I3', 'NT_GER', 'QE_I01', 'QE_I02', 'QE_I08']
-
-    # Gráficos de barras
-    # for i in range(2):
-    #     plot_bar_graph(pd.read_csv(f"{dirname}/{cols[i]}.csv", sep), cols[i])
-
-    # Histograma
-    # plot_histogram(pd.read_csv(f"{dirname}/{cols[2]}.csv", sep).apply(str_num_to_float))
-
-    # Gráficos de pizza
-    ans_labels = [
-        {
-            'A': 'Solteiro(a)', 'B': 'Casado(a)', 'C': 'Separado(a) judicialmente/divorciado(a)',
-            'D': 'Viúvo(a)', 'E': 'Outro'
-        },
-        {
-            'A': 'Branca', 'B': 'Preta', 'C': 'Amarela', 'D': 'Parda',
-            'E': 'Indígena', 'F': 'Não quero declarar'
-        }
+    dirname_file_full = f'{dirname}/microdados_enade_2018_edit.csv'
+    df = pd.read_csv(dirname_file_full, sep=';', decimal=',')
+    bar_graphs: List[Graph2D] = [
+        Graph2D(
+            'CO_RS_I1', None, 'Percepção de dificuldade da prova', 'Nível de dificuldade', 'Número de votos',
+            {
+                'A': 'Muito fácil', 'B': 'Fácil', 'C': 'Médio', 'D': 'Difícil',
+                'E': 'Muito difícil', '.': 'S/ resp.', '*': 'Anulada'
+            }
+        ),
+        Graph2D(
+            'CO_RS_I3', None, 'Percepção de duração da prova', 'Percepção de duração', 'Número de votos',
+            {
+                'A': 'Muito longa', 'B': 'Longa', 'C': 'Adequada', 'D': 'Curta',
+                'E': 'Muito curta', '.': 'S/ resp.', '*': 'Anulada'
+            }
+        ),
+    ]
+    histograms: List[Graph2D] = [
+        Graph2D(
+            'NT_GER', None, 'Histograma de notas', 'Nota bruta da prova', 'Quantidade de notas'
+        )
+    ]
+    pie_graphs: List[Graph2D] = [
+        Graph2D(
+            'QE_I01', None, 'Distribuição por estado civil', 'Estado civil', 'Número de candidatos',
+            {'A': 'Solteiro', 'B': 'Casado', 'C': 'Separado / divorciado', 'D': 'Viúvo', 'E': 'Outro'}
+        ),
+        Graph2D(
+            'QE_I02', None, 'Distribuição por cor/raça', 'Cor / raça', 'Número de candidatos',
+            {'A': 'Branca', 'B': 'Preta', 'C': 'Amarela', 'D': 'Parda', 'E': 'Indígena', 'F': 'Não declarada'}
+        ),
+    ]
+    boxplot_graphs: List[Graph2D] = [
+        Graph2D(
+            'QE_I08', 'NT_GER', 'Nota bruta por rendimento familiar', 'Rendimento familiar (Em salário mínimo)',
+            'Nota bruta',
+            {
+                'A': 'Até 1.5', 'B': '1.5 a 3', 'C': '3 a 4.5', 'D': '4.5 a 6',
+                'E': '6 a 10', 'F': '10 a 30', 'G': 'Mais de 30'
+            }
+        ),
+        Graph2D(
+            'QE_I04', 'NT_GER', 'Nota bruta por escolaridade do pai', 'Escolaridade do pai', 'Nota bruta',
+            {
+                'A': 'Nenhuma', 'B': '1ª a 4ª série', 'C': '5ª a 8ª série',
+                'D': 'Ensino médio', 'E': 'Graduação', 'F': 'Pós-graduação'
+            }
+        ),
+    ]
+    graphs_bonus: List[Graph2D] = [
+        Graph2D(
+            'QE_I15', 'NT_GER', 'Nota bruta por ação afirmativa', 'Ação afirmativa', 'Nota bruta média',
+            {
+                'A': 'Não', 'B': 'Étnico-racial', 'C': 'Renda', 'D': 'E. pública ou bolsa',
+                'E': 'Múltip. critérios', 'F': 'Sistema diferente'
+            }
+        ),
+        Graph2D(
+            'QE_I17', 'NT_GER', 'Nota bruta por escola (ensino médio)', 'Tipo de escola', 'Nota bruta média',
+            {
+                'A': 'Pública', 'B': 'Particular', 'C': 'Exterior',
+                'D': 'Púb. (majorit.)', 'E': 'Part. (majorit.)',
+                'F': 'Brasil e exterior'
+            }
+        ),
+        Graph2D(
+            'QE_I23', 'NT_GER', 'Nota bruta por horas de estudo semanal', 'Horas de estudo', 'Nota bruta média',
+            {'A': '0 - só vê às aulas', 'B': '1 a 3', 'C': '4 a 7', 'D': '8 a 12', 'E': '+12'}
+        )
     ]
 
-    for i in range(2):
-        col_name = cols[i + 3]
-        df = pd.read_csv(f"{dirname}/{col_name}.csv", sep)
-        count: Dict[str, int] = df[col_name].value_counts()
-        values = [count[ans] for ans in ans_labels[i]]
-        labels = [ans_labels[i][ans] for ans in ans_labels[i]]
-        plot_pie_graph(labels, values)
+    # Gráficos de barras
+    for g in bar_graphs:
+        plot_bar_graph(df, g.x_col, g.title, g.xlabel, g.ylabel, g.label_categories)
 
-    # Obs: dados NaN ou NA é sinônimo de sem resposta
+    # Histograma
+    for h in histograms:
+        plot_histogram(df[h.x_col], h.title, h.xlabel, h.ylabel)
+
+    # Gráficos de pizza
+    for g in pie_graphs:
+        count: Dict[str, int] = df[g.x_col].value_counts()
+        values = [count[ans] for ans in g.label_categories]
+        labels = list(g.label_categories.values())
+        plot_pie_graph(labels, values, g.title)
+
+    # Gráficos boxplot
+    for g in boxplot_graphs:
+        plot_boxplot(df, g.x_col, g.y_col, g.title, g.xlabel, g.ylabel, g.label_categories)
+
+    # Gráficos boxplot (extras)
+    for g in graphs_bonus:
+        plot_boxplot(df, g.x_col, g.y_col, g.title, g.xlabel, g.ylabel, g.label_categories)
+
     return 0
 
 
